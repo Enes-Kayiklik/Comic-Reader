19cdfbfa15c6ffa9070ac527d66dffb14272be9b
Enes Kayiklik <eneskayiklik@gmail.com>
2020-10-09 08:58:23 +0300

    changed search view style

--- app/src/main/java/com/eneskayiklik/comicreader/ui/main/bookshelf/BookShelfFragment.kt
+++ app/src/main/java/com/eneskayiklik/comicreader/ui/main/bookshelf/BookShelfFragment.kt
@@ -5,7 +5,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -19,22 +19,35 @@ import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator
 import kotlinx.android.synthetic.main.fragment_bookshelf.*
 
 @AndroidEntryPoint
-class BookShelfFragment : Fragment(R.layout.fragment_bookshelf), SearchView.OnQueryTextListener {
+class BookShelfFragment : Fragment(R.layout.fragment_bookshelf) {
     private val bookShelfViewModel: BookShelfViewModel by viewModels()
     private lateinit var adapter: BookAdapter
     private lateinit var bookList: List<Book>
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupUtils()
+        setupSearch()
+    }
+
+    private fun setupSearch() {
+        edtSearchView.doOnTextChanged { text, _, _, _ ->
+            if (this::bookList.isInitialized) {
+                adapter.bookList = bookList.filter { it.name.contains(text ?: "", true) }
+                adapter.notifyDataSetChanged()
+            }
+        }
+    }
+
+    private fun setupUtils() {
         val appCompatActivity = activity as AppCompatActivity
         appCompatActivity.setSupportActionBar(toolbarBookshelfFragment)
         setHasOptionsMenu(true)
-
     }
 
     override fun onStart() {
         super.onStart()
         if (bookShelfViewModel.allBooks.value.isNullOrEmpty())
-            setupObserver()
+            setupObserver().also { changeLayoutVisibilities(true) }
         else
             setupRecyclerView(bookShelfViewModel.allBooks.value)
     }
@@ -42,11 +55,11 @@ class BookShelfFragment : Fragment(R.layout.fragment_bookshelf), SearchView.OnQu
     private fun setupObserver() {
         bookShelfViewModel.allBooks.observe(this.viewLifecycleOwner, Observer {
             setupRecyclerView(it)
+            changeLayoutVisibilities(false)
         })
     }
 
     private fun setupRecyclerView(books: List<Book>?) {
-        changeLayoutVisibilities()
         bookList = books ?: emptyList()
         adapter = BookAdapter(bookList)
         recyclerViewBooks.itemAnimator = SlideInLeftAnimator()
@@ -55,29 +68,18 @@ class BookShelfFragment : Fragment(R.layout.fragment_bookshelf), SearchView.OnQu
             GridLayoutManager(this.requireContext(), 3, GridLayoutManager.VERTICAL, false)
     }
 
-    private fun changeLayoutVisibilities() {
-        loadingView.makeInvisible()
-        recyclerViewBooks.makeVisible()
-    }
-
-    override fun onQueryTextSubmit(p0: String?): Boolean {
-        return true
-    }
-
-    override fun onQueryTextChange(text: String): Boolean {
-        if (this::bookList.isInitialized) {
-            val selectedBooks = bookList.filter { it.name.contains(text, true) }
-            adapter.bookList = selectedBooks
-            adapter.notifyDataSetChanged()
+    private fun changeLayoutVisibilities(isVisible: Boolean) {
+        if (isVisible) {
+            loadingView.makeVisible()
+            recyclerViewBooks.makeInvisible()
+        } else {
+            loadingView.makeInvisible()
+            recyclerViewBooks.makeVisible()
         }
-        return true
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.search_menu, menu)
-        val search = menu.findItem(R.id.search)
-        val searchView = search?.actionView as SearchView
-        searchView.setOnQueryTextListener(this)
     }
 }
